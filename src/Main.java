0ed038c670c600ed99230d8a1740672a5e60e4ff
Raghavendra Shirashyad <raghavendra.shirashyad@ecanarys.com>
2025-03-20 17:25:18 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+// src/Main.java
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Use Greeter class
+        Greeter greeter = new Greeter(name);
+        System.out.println(greeter.sayHello());
+
+        // File I/O
+        System.out.println("\nWriting to file...");
+        FileHandler.writeToFile("data.txt", "Hello, " + name);
+
+        System.out.println("Reading from file...");
+        String content = FileHandler.readFromFile("data.txt");
+        System.out.println("File content: " + content);
+
+        scanner.close();
+    }
+}
