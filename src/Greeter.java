7e0d3b4af39ca1a513ca15f84c568296a825d6bf
Raghavendra Shirashyad <raghavendra.shirashyad@ecanarys.com>
2025-03-20 17:25:52 +0530

    Create Greeter.java

---
+++ src/Greeter.java
@@ -0,0 +1,15 @@
+// src/Greeter.java
+
+public class Greeter {
+    private String name;
+
+    // Constructor
+    public Greeter(String name) {
+        this.name = name;
+    }
+
+    // Method to generate greeting message
+    public String sayHello() {
+        return "Hello, " + name + "! Welcome to Java.";
+    }
+}
